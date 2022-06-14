c1186de31807af5e0d5351a18e46a8c9ddb59163
SpiderTheSpider <102829476+SpiderTheSpider@users.noreply.github.com>
2022-06-14 12:40:38 -0400

    Update tes3matlab.m

--- tes3matlab.m
+++ tes3matlab.m
@@ -15,11 +15,12 @@
 %       to show the recommended call syntax and order
 Breaker = false(99,1);
 %-------------------%
-Breaker(01) = 0;	% Load/extract data from specified list of files
-Breaker(02) = 1;	% Merge data from multiple files into 1 data structure
-Breaker(03) = 0;    % Generate specified world map(s)
-Breaker(04) = 0;    % Draw specified map(s)
-Breaker(05) = 0;    % Display and/or export specified map(s)
+Breaker(01) = 1;	% Load/extract data from specified list of files
+Breaker(02) = 1;    % Clean extracted datums
+Breaker(03) = 1;	% Merge data from multiple files into 1 data structure
+Breaker(04) = 1;    % Generate specified world map(s)
+%Breaker(05) = 0;    % Draw specified map(s) [IN PROGRESS]
+%Breaker(06) = 0;    % Display and/or export specified map(s) [IN PROGRESS]
 %-------------------%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -32,13 +33,13 @@ Breaker(05) = 0;    % Display and/or export specified map(s)
     opts = struct;
     
     % Project name (for backup and exported file naming)
-    opts.project_name = 'vanilla';
+    opts.project_name = 'tamriel_rebuilt';
     
     % Text file containing list of TES3 files to extract data from
     % NOTE: Make sure the load order is the same as what is used in-game!
     % NOTE: Make sure that each file name is headed by the full directory
     %       e.g. C:\games\morrowind\mods\mod_name.esp
-    opts.list_file = '.\lists\filelist_vanilla.txt';
+    opts.list_file = '.\lists\filelist_tr.txt';
     
     % Directory to look for land texture files
     % NOTE: This, and all other defined directories, need to be manually
@@ -48,20 +49,16 @@ Breaker(05) = 0;    % Display and/or export specified map(s)
     %       to generate a list of land textures used by the imported
     %       file(s), then use DDS Converter (available at 
     %       http://www.ddsconverter.com/) to convert these textures to PNGs
-    opts.ltex_dir = '.\images\ltex_pngs\';
+    opts.ltex_dir = '.\images\CreateMaps\tr\';
     
     % CreateMaps output directory
     % NOTE: This assumes that the same files used in the CreateMaps output
     %       are what is listed in input_file
-    opts.createmaps_dir = '.\images\createmaps\vanilla\';
-    
-    % GridMap file
-    % NOTE: Unused at the moment, but could be another source of info...
-    % NOTE: Open and save the master file as a .TIFF so MATLAB can read it!
-    opts.gridmap_file = '.\images\gridmap\Tamriel_MasterImage_v061122.tiff';
+    %opts.createmaps_dir = '.\images\createmaps\vanilla\';
+    opts.createmaps_dir = 'C:\Games\Steam\steamapps\common\Morrowind\Maps\';
 
     % Export directory, where results are saved to
-    opts.export_dir = '.\exports\vanilla\';
+    opts.export_dir = '.\exports\tr\';
     
     % Backup directory, where extracted data is stored so you don't have to
     % run the extraction every time
@@ -74,14 +71,13 @@ Breaker(05) = 0;    % Display and/or export specified map(s)
     opts.allow_skip = false;    % Allow skipping the extraction for a data file if an error is encountered
     opts.bkup_data  = true;     % Back up extracted data so you don't have to extract it from the file every time
     opts.get_vclr	= false;	% Extract vertex color map of LAND entries
-    opts.get_vnml	= true; 	% Extract vertex normal map of LAND entries
-    opts.map_to_gen = {'ALL'};  % Choose 'ALL' or individual maps (see help entry of tes3matlab_gen_maps for more info)
-    opts.dims.cell_bgrid = 16;  % Used to generate a 'block map', a map where dx=dy=dz (think Minecraft)
+    opts.get_vnml	= false; 	% Extract vertex normal map of LAND entries
+    opts.map_list   = {'ALL'};  % Choose 'ALL' or individual maps (see help entry of tes3matlab_gen_maps for more info)
     
     % Global constants
     % NOTE: YOU SHOULD NOT NEED TO CHANGE THESE, but if you want to adapt
     % this code for TES4 or something else, these values may change!
-    opts.dims = struct;             % Contains the various dimension definitions
+    %opts.dims = struct;             % Contains the various dimension definitions
     opts.dims.cell_gu    = 8192;	% Length of cell edge in game units
     opts.dims.cell_yd    = 128;     % Length of cell edge in yards
     opts.dims.h_inc      = 8;       % One heightmap increment (in vertical) is this many game units
@@ -94,7 +90,6 @@ Breaker(05) = 0;    % Display and/or export specified map(s)
 % END OPTIONS
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if(Breaker(01)); file_data = tes3matlab_extract(opts);                  end
-if(Breaker(02)); merged_data = tes3matlab_merge(file_data,opts);        end
-if(Breaker(03)); map_data = tes3matlab_genmaps(merged_data,opts);       end
-if(Breaker(04)); map_imgs = tes3matlab_drawmaps(map_data,opts);         end
-if(Breaker(05)); tes3matlab_exportmaps(map_imgs,opts);                  end
+if(Breaker(02)); file_data = tes3matlab_clean(file_data);               end
+if(Breaker(03)); merged_data = tes3matlab_merge(file_data,opts);        end
+if(Breaker(04)); map_data = tes3matlab_genmaps(merged_data,opts);       end
