c985ea988d257c7a9b9c0964871e3a9f44cdbda7
SpiderTheSpider <102829476+SpiderTheSpider@users.noreply.github.com>
2022-06-27 10:18:39 -0400

    Delete functions directory

--- functions/median_color.m
+++
@@ -1,8 +0,0 @@
-function med_color = median_color(input_image)
-% Finds median color of image
-    med_color = zeros(1,3);
-    for i=1:3
-        tmp = input_image(:,:,i);
-        med_color(i) = median(tmp(:));
-    end
-end
